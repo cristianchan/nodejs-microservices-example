e287a2709fe3e45bfe1ae576778aea492a30c720
Cristian Chan <cchan@osigu.com>
2018-04-17 12:33:56 -0600

    Add slack event

--- server/slackClient.js
+++ server/slackClient.js
@@ -1,8 +1,16 @@
 'use strict'
 
-const {RTMClient} = require('@slack/client');
+const RtmClient = require('@slack/client').RtmClient;
+const CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
 
-module.exports.init = function slackClient(token, logLevel) {
-    const rtm = new RTMClient(token);
-    return rtm;
+function handleOnAuthenticated(rtmStartData) {
+    console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`);
 }
+
+module.exports.addAuthenticatedHandler = function addAuthenticatedHandler(rtm, handler) {
+    rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, handler);
+}
+
+module.exports.init = function slackClient(token) {
+    return new RtmClient(token);
+};
